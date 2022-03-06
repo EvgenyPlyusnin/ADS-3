2edae3a5a4564b428693b2b37447fe3de5586615
EvgenyPlyusnin <zhenja.plyusnin@yandex.ru>
2022-03-06 17:07:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,61 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int index = 0;
+    int result = 0;
+    int start = 0;
+    int stop = size;
+    for (int i = 2; i < size; i+=2) {
+        if (arr[size / i] == value) {
+            index = size / i;
+            break;
+        }
+        if (arr[size / i] < value) {
+            start = size / i;
+        } else {
+            stop = size / i;
+        }
+    }
+    while (true) {
+        if (index == 0) {
+            for (int i = 0; i < size; i++) {
+                if (arr[i] == value) {
+                    result++;
+                } else {
+                    break;
+                }
+            }
+            break;
+        } else if (index == size) {
+            for (int i = size; i >= 0; i--) {
+                if (arr[i] == value) {
+                    result++;
+                } else {
+                    break;
+                }
+            }
+            break;
+        } else {
+            int ind1 = index;
+            int ind2 = index - 1;
+            while (true) {
+                if (ind1 < size && arr[ind1] == value) {
+                    result++;
+                    ind1++;
+                } else {
+                    break;
+                }
+            }
+            while (true) {
+                if (ind2 >= 0 && arr[ind2] == value) {
+                    result++;
+                    ind2--;
+                } else {
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    return result;
 }
